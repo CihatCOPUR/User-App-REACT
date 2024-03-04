09b550ed47b86c4e840003457180d220f5767306
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2024-03-04 19:59:15 +0300

    mongodb

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+//template engine
+app.set('view engine', 'ejs');
 
-
-
+//middlewares
 app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('index');
+})
+app.get('/add', (req, res) => {
+  res.render('add');
+})
+app.get('/about', (req, res) => {
+  res.render('about');
 })
 
 
