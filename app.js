f5f5341864750952f25dcfd0ba92a533aa40d2d8
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2024-03-11 16:27:44 +0300

    photo upload

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fileupload = require('express-fileupload');
 const path = require('path');
 const ejs = require('ejs');
+const fs = require('fs');
 const Photo = require('./models/Photo');
-const mongoose = require('mongoose');
 
 const app = express();
 
@@ -15,23 +17,50 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileupload());
 
 
 app.get('/', async (req, res) => {
-  const photos = await Photo.find()
+  const photos = await Photo.find().sort('-dateCreated')
   res.render('index', { photos });
 })
 app.get('/add', (req, res) => {
   res.render('add');
+
+});
+
+app.get('/photos/:id', async (req, res) => {
+  // res.render('');
+  // console.log(req.params.id)
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', { photo });
 })
 
 app.get('/about', (req, res) => {
   res.render('about');
 })
 
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+app.post('/photos', (req, res) => {
+
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name
+    });
+
+    res.redirect('/');
+  })
+
+
 })
 
 const PORT = 8000;
